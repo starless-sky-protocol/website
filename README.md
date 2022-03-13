466ffa6101c92aa276c440f9c88865dead04ee71
Gabriel Scatolin <gscatolin@outlook.com>
2022-03-13 15:23:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Starless Sky website
+
+Official Starless Sky website at [starless-sky.org](https://starless-sky.org). The site was written in PHP, without any PHP framework, being possible to run on any Apache, nginx or Litespeed server.
+
+Credits:
+- [Bootstrap 4](https://getbootstrap.com/) front-end framework.
+- [Line Awesome.](https://icons8.com/line-awesome)
+- [Icons8.](https://icons8.com/)
---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "head.php"; ?>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <img src="/img/starless sky alata.png" style="width: 200px; height: auto;" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://docs.starless-sky.org/">Documentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/starless-sky-protocol">Repository</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://app.starless-sky.org/">App</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="home">
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="col-md-6 col-sm-12">
+                    <h1 class="display-4 w-100">
+                        Where privacy matters.
+                    </h1>
+                    <p class="lead">
+                        Starless Sky is an open sourec project which aims to bring privacy, anonymity and security to the internet.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 d-flex">
+                    <img src="/img/Starless Sky.png" class="my-auto ml-auto" style="width: 50%; height: auto;" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-sm-12">
+                    <p class="text-center font-lato lead">
+                        Starless Sky is a network protocol that anyone can have
+                        their own network, keeping the security and authenticity of information simple and secure.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-12 mb-3">
+                    <i class="las la-question la-6x mb-3 text-brand"></i>
+                    <h3>Why?</h3>
+                    <p class="font-lato">
+                        In a centralized internet it is difficult to know what really
+                        happens to your data. We live on promises that protect our
+                        information, but we don't know how it's working behind the
+                        scenes. Starless Sky is here to change that.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-3">
+                    <i class="las la-exclamation la-6x mb-3 text-brand"></i>
+                    <h3>How?</h3>
+                    <p class="font-lato">
+                        Using advanced cryptography and blockchain, it is possible
+                        to validate the information that is on the network separately
+                        from it. Validation is always made by the clients,
+                        that is, any integrity violation is alerted to the user.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-5 border-top">
+        <div class="container">
+            <h3 class="text-center mb-5">Starless Sky differentials</h3>
+            <div class="row">
+                <div class="col-md-6 col-sm-12 mb-5">
+                    <img src="/img/icon/icons8-hide-100.png" class="mb-3" style="width: 80px; height: auto;" />
+                    <h3>Anonymous</h3>
+                    <p class="font-lato">
+                        Addresses are not deterministic and do not reveal
+                        the real identity of their bearers unless they want to do so.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-5">
+                    <img src="/img/icon/icons8-lock-100.png" class="mb-3" style="width: 80px; height: auto;" />
+                    <h3>Secure</h3>
+                    <p class="font-lato">
+                        Messages and contracts protected by asymmetric
+                        and symmetric encryption, making it impossible for an intruder to read or modify them.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-5">
+                    <img src="/img/icon/icons8-blockchain-technology-100.png" class="mb-3" style="width: 80px; height: auto;" />
+                    <h3>Incorruptible</h3>
+                    <p class="font-lato">
+                        The network is secured by blockchain and
+                        validated by its own users. Any misplaced letter
+                        is immediately alerted to the user.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-5">
+                    <img src="/img/icon/icons8-contract-100.png" class="mb-3" style="width: 80px; height: auto;" />
+                    <h3>Smart Contracts</h3>
+                    <p class="font-lato">
+                        Send and receive secure contracts
+                        only between you and the issuer, and no one else. Blockchain-protected smart signatures.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-5">
+                    <img src="/img/icon/icons8-fast-forward-100.png" class="mb-3" style="width: 80px; height: auto;" />
+                    <h3>Instantaneous</h3>
+                    <p class="font-lato">
+                        Messages sent and received instantly, keeping you safe and secure.
+                    </p>
+                </div>
+                <div class="col-md-6 col-sm-12 mb-5">
+                    <img src="/img/icon/icons8-easy-100.png" class="mb-3" style="width: 80px; height: auto;" />
+                    <h3>Easy</h3>
+                    <p class="font-lato">
+                        Easy installation and use. No technical knowledge is required to start sending messages or contracts.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-5 border-top">
+        <div class="container">
+            <h3 class="text-center mb-3">You make your own internet</h3>
+            <div class="row justify-content-center">
+                <div class="col-md-4 col-sm-10">
+                    <a href="https://app.starless-sky.org/" class="btn btn-lg btn-primary btn-block mb-3">Start using Starless Sky</a>
+                    <a href="https://docs.starless-sky.org/" class="btn btn-link btn-block text-brand">Want to create your own network?</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
